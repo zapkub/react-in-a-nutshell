8abd42220de950703eedc550cb87bceb906e0f3c
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2016-10-28 11:36:52 +0700

    Boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div>
+                App
+            </div>
+        )
+    }
+}
+export default App;
