ff026c097b0dbb014af0e4cec1501e37591484c3
Rungsikorn Rungsikavanich <rungsikorn@icloud.com>
2018-11-15 17:14:09 +0700

    update code style

--- packages/todo-hooks-context/src/App.js
+++ packages/todo-hooks-context/src/App.js
@@ -1,32 +1,32 @@
 import React from "react";
-import "todomvc-common/base.css";
-import "todomvc-app-css/index.css";
-import { TodoStoreProvider, TodoStoreConsumer } from "./store";
+import { TodoStoreProvider } from "./store";
+
 import { TodoFilters } from "./components/TodoFilters";
 import { TodoInput } from "./components/TodoInput";
 import { TodoList } from "./components/TodoList";
 import Footer from "./components/Footer";
 import { TodoCount } from "./components/TodoCount";
 
+import "todomvc-common/base.css";
+import "todomvc-app-css/index.css";
+
 export default () => {
   return (
     <TodoStoreProvider>
-      <>
-        <div className="App todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <TodoInput />
-          </header>
-          <section className="main">
-            <TodoList />
-          </section>
-          <footer className="footer">
-            <TodoCount />
-            <TodoFilters />
-          </footer>
-        </div>
-        <Footer />
-      </>
+      <div className="App todoapp">
+        <header className="header">
+          <h1>{"todos"}</h1>
+          <TodoInput />
+        </header>
+        <section className="main">
+          <TodoList />
+        </section>
+        <footer className="footer">
+          <TodoCount />
+          <TodoFilters />
+        </footer>
+      </div>
+      <Footer />
     </TodoStoreProvider>
   );
 };
