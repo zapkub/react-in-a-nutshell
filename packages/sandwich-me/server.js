8088396ee985d2d5a579bf766831a65cb93bab02
Rungsikorn Rungsikavanich <rungsikorn@icloud.com>
2018-11-29 21:16:13 +0700

    add configuration chiocies

---
+++ packages/sandwich-me/server.js
@@ -0,0 +1,19 @@
+
+
+const app = require('express')()
+
+const Joi = require('joi');
+
+
+app.post('/order', () => {
+
+
+
+
+
+})
+
+
+
+
+app.listen(3001)
---
+++ packages/sandwich-me/src/configuration.js
@@ -0,0 +1,51 @@
+
+const menuOptions = [
+  'Black Forest Ham',
+  'Chicken & Bacon Ranch Melt',
+  'Italian B.M.T.'
+]
+
+const breadOptions = [
+  "Parmesan Oregano",
+  "Italian",
+  "9-Grain Wheat,",
+  "Italian Herbs & Cheese",
+  "Monterey Cheddar",
+  "9-Grain Honey Oat"
+];
+
+const veggieOptions = [
+  "Cucumbers",
+  "Green Peppers",
+  "Lettuce",
+  "Red Onions",
+  "Spinach",
+  "Tomatoes",
+  "Banana Peppers",
+  "Jalapeños",
+  "Black Olives",
+  "Pickles",
+  "vocado",
+  "Carrots",
+  "Guacamole",
+  "Sweet Peppers"
+];
+
+const saucesOptions = [
+  "Chipotle Southwest",
+  "Light or Regular Mayonnaise",
+  "Ranch",
+  "Oil",
+  "Subway® Vinaigrette",
+  "Mustard",
+  "Vinegar",
+  "Sweet Onion",
+  "Barbecue",
+  "Buffalo",
+  "Creamy Italian",
+  "Golden Italian",
+  "Honey Mustard",
+  "Savory Caesar",
+  "Sriracha",
+  "Tzatziki Cucumber"
+];
